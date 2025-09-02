5139f95979047a900b12f34edcda077919050b26
DESKTOP-P3GC6VP\mingdv <vuminh11235@gmail.com>
2025-09-02 18:25:47 +0700

    update metadata for Menu Order application

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Menu Order',
+  description: 'Created by mingdv',
+  generator: 'fb.com/mingdv',
 }
 
 export default function RootLayout({
